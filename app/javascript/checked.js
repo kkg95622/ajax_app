ef4f44aa7dc7e4e0d8186544d177fb45d6b7d754
kkg95622 <kkg95622@yahoo.co.jp>
2020-07-31 21:31:35 +0900

    既読機能のフロント側の記述

---
+++ app/javascript/checked.js
@@ -0,0 +1,47 @@
+function check() {
+  console.log("js")
+  const posts = document.getElementsByClassName("post");
+ 
+  postsA = Array.from(posts);
+  console.log(postsA)
+ 
+  postsA.forEach(function (post) {
+    console.log(post)
+      if (post.getAttribute("data-load") != null) {
+       return null;
+     }
+     post.setAttribute("data-load", "true");
+    post.addEventListener("click", (e) => {
+      
+      const postId = post.getAttribute("data-id");
+      const XHR = new XMLHttpRequest();
+      console.log(postId)
+ 
+      XHR.open("GET", `/posts/${postId}`, true);
+      XHR.responseType = "json";
+      XHR.send();
+      XHR.onload = () => {
+        const item = XHR.response.post;
+        if (item.checked === true) {
+          post.setAttribute("data-check", "true");
+        } else if (item.checked === false) {
+          post.removeAttribute("data-check");
+        }
+ 
+        if (XHR.status != 200) {
+          alert(`Error ${XHR.status}: ${XHR.statusText}`); 
+        } else {
+          return null;
+        }
+      };
+
+      XHR.onerror = () => {
+        alert("Request failed");
+      };
+ 
+      e.preventDefault();
+    });
+  });
+ }
+ 
+ setInterval(check, 1000);
